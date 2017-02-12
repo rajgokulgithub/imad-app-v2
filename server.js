ee2dcd3e616adcd91e01f4daed332e9eac65a6a8
rajgokulgithub <rajgokul2202@gmail.com>
2017-02-12 21:32:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,69 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var plays = {
+    'play1':{
+        title:'First Play',
+        content:`<p>
+             im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........
+         </p>
+         <p>
+             im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........im first...........
+         </p>`
+         },
+    'play2':{
+           title:'Second Play',
+           content: `<p>
+             im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....
+         </p>
+         <p>
+             im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second.....im second..............im second.....im second.....im second.....
+         </p>`
+           
+        },
+    'play3':{
+       title:'Third Play',
+       content:`<p>
+             im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... 
+         </p>
+         <p>
+             im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... im third....... 
+             </p>`
+    }
+        };
+
+
+
+function createtemplate(data)
+{
+     var title = data.title;
+     var content = data.content;
+     var template = `
+    <html>
+      <head>
+       <title>${title}</title> 
+       <link href="/ui/style.css" rel="stylesheet" />
+       <meta name="viewport" content="width=device-width,initialscale=1"/>
+
+    </head>
+    <body>
+      <div>
+         <a href='/'>Home</a>
+      </div>
+      <hr/>
+      <h2>Guess what!</h2>
+      <div>
+         Im Doing This
+      </div>
+     
+      <div>
+          ${content}
+      </div>
+     
+    </body>
+</html>`;
+return template;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,18 +80,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/play1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'play1.html'));
+app.get('/:play', function (req, res) {
+  res.send(createtemplate(plays[play]));
 });
 
-app.get('/play2', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'play2.html'));
 
-});
-
-app.get('/play3', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'play3.html'));
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
