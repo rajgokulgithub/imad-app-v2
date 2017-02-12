d8b4431fccdba47b77829134e3ea7be457374351
rajgokulgithub <rajgokul2202@gmail.com>
2017-02-12 22:59:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@ var img = document.getElementById('bomma');
 var marginLeft=0;
 img.onclick = function(){
      var mes = document.getElementById("text");
-     mes.innerHTML = "What Have Done!!!!!!!";
+     mes.innerHTML = "What Have You Done!!!!!!!";
      
      var inteval = setInterval(moveRight,50);
 };
 
 function moveRight(){
     marginLeft = marginLeft+10;
-    img.style.marginLeft = marginLeft+"px;"
+    img.style.marginLeft = marginLeft+'px';
 }
