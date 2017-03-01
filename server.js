b29c51db95ad9c7c25e0d020e6ff8d00539c4290
rajgokulgithub <rajgokul2202@gmail.com>
2017-03-01 12:56:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,14 +68,17 @@ function createtemplate(data)
 </html>`;
 return template;
 }
-*/app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+*/
 var counter =0;
 app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
