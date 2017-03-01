397957907e8d58084b9259406d0cd5eb381d3b80
rajgokulgithub <rajgokul2202@gmail.com>
2017-03-01 11:39:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,11 @@ app.get('/:play', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
+var counter =0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 
 
