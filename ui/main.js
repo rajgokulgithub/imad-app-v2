33b92149902fe592f663f26b4b87351195ffb31c
rajgokulgithub <rajgokul2202@gmail.com>
2017-03-01 13:09:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,11 +16,10 @@ img.onclick = function(){
 
 //likes for a pic 
 console.log('Loaded!');
-/*var button = document.getElementById('likes');
+var button = document.getElementById('likes');
 var likes = 0;
 button.onclick= function(){
       likes=likes+1;
       var span = document.getElementById('like');
       span.innerHTML = likes.toString(); 
 };
-*/
