5a4f6efb67a965fda90d2f13da8182429d23bbb3
rajgokulgithub <rajgokul2202@gmail.com>
2017-03-01 11:40:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,11 @@ return template;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var counter =0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -87,11 +91,7 @@ app.get('/:play', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-var counter =0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 
 
 
